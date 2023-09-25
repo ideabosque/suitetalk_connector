270fd901af15ad0bb88ff5e1c9d2487b800da5cc
DESKTOP-ENENB64\Bibo W\github <ideabosque@gmail.com>
2023-09-25 13:31:59 -0700

    Added search_more_with_id  into advance_search.

--- suitetalk_connector/soapconnector.py
+++ suitetalk_connector/soapconnector.py
@@ -2496,9 +2496,9 @@ class SOAPConnector(object):
         return None
 
     def advance_search(self, entity_type, saved_search_id, **kwargs):
-        if kwargs.get("page_index"):
+        if kwargs.get("search_id") and kwargs.get("page_index"):
             return self.search_more_with_id(
-                saved_search_id, kwargs.get("page_index"), advance=True
+                kwargs.get("search_id"), kwargs.get("page_index"), advance=True
             )
 
         SearchDateField = self.get_data_type("ns0:SearchDateField")
